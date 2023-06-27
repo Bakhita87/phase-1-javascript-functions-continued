03d30d4b92b39f94fa0c58b120fde1b96b095825
Bakhita87 <bakhitaotieno@gmail.com>
2023-06-27 11:54:30 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // code your solution here
+function saturdayFun (param1 = "roller-skate"){
+    return `This Saturday, I want to ${param1}!`;
+    
+}
+
+saturdayFun("roller-skate");
+
+
+function mondayWork (param2 = "go to the office"){
+    return  `This Monday, I will ${param2}.`;
+}
+
+mondayWork("gototheoffice");
+
+function wrapAdjective (wrap = "*"){
+ return inputFunction(wrap)
+    
+}
+
+
+
+function wrapAdjective(adjective= "*"){
+    return function(sentence= "a hard worker"){
+    return `You are ${adjective}${sentence}${adjective}!`
+    }}
+
+
+
+
+
